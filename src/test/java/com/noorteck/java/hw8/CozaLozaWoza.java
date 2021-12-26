68e17f9b1c2c8c1d2d3f0fcb6ca1ef3d00d6efb9
Adam Abshire <chef.abshire@gmail.com>
2021-12-26 10:54:13 -0500

    Re-evaluated the coza woza requirements. now the special corresponding word appears only when the number is divisible ONLY by each case, and only that case.

--- src/test/java/com/noorteck/java/hw8/CozaLozaWoza.java
+++ src/test/java/com/noorteck/java/hw8/CozaLozaWoza.java
@@ -7,11 +7,11 @@ public class CozaLozaWoza {
 
 			if (i % 3 == 0 && i % 5 == 0) {
 				System.out.println("CozaLoza");
-			} else if (i % 3 == 0) {
+			} else if (i % 3 == 0 && i % 5 != 0 && i % 7 != 0) {
 				System.out.println("Coza");
-			} else if (i % 5 == 0) {
+			} else if (i % 5 == 0 && i % 3 != 0 && i % 7 != 0) {
 				System.out.println("Loza");
-			} else if (i % 7 == 0) {
+			} else if (i % 7 == 0 && i % 3 != 0 && i % 5 != 0) {
 				System.out.println("Woza");
 			} else {
 				System.out.println(i);
