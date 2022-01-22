ee60e5ab4c675bb3c2615f65febd49d2d2cada26
Adam Abshire <chef.abshire@gmail.com>
2022-01-22 12:36:53 -0500

    comments

--- src/test/java/com/noorteck/java/hw20/Question16.java
+++ src/test/java/com/noorteck/java/hw20/Question16.java
@@ -12,7 +12,7 @@ public class Question16 {
 
 	String concatString(String strOne, String strTwo) {
 		String result = "";
-		// first character of strTwo are the same
+		// see if last character of strOne & first character of strTwo are the same
 		int lastIndex = strOne.length() - 1;
 		if (strOne.charAt(lastIndex) == strTwo.charAt(0)) {
 			// delete the last character of strOne before concatenating
@@ -28,13 +28,12 @@ public class Question16 {
 
 	public static void main(String[] args) {
 		Question16 obj = new Question16();
-		
+
 		String resultOne = obj.concatString("java", "awesome");
 		String resultTwo = obj.concatString("food", "door");
 		String resultThree = obj.concatString("java", "training");
 		String resultFour = obj.concatString("Pro", "ogress");
-		
-		
+
 		System.out.println(resultOne);
 		System.out.println(resultTwo);
 		System.out.println(resultThree);
