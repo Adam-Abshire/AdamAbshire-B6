e6fd55fa5fe10d511f47fa4acdba78917efec547
Adam Abshire <chef.abshire@gmail.com>
2022-03-27 16:35:02 -0400

    123

---
+++ src/test/java/interview/questions/Question24.java
@@ -0,0 +1,40 @@
+package interview.questions;
+import java.util.ArrayList;
+
+public class Question24 {
+	
+	public static ArrayList<Integer> sortInteger(ArrayList<Integer> array) {
+		
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for (int i = 0; i <= array.size(); i++) {
+			
+		}
+		
+		return result;
+		
+	}
+	
+	public static ArrayList<String> sortString(ArrayList<String> array) {
+		
+		ArrayList<String> result = new ArrayList<String>();
+		
+		return result;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> arrayListOne = new ArrayList<Integer>();
+		ArrayList<String> arrayListTwo = new ArrayList<String>();
+		
+		ArrayList<Integer> resultOne = sortInteger(arrayListOne);
+		ArrayList<String> resultTwo = sortString(arrayListTwo);
+		
+		System.out.println(resultOne);
+		System.out.println(resultTwo);
+		
+	}
+	
+}
+
+// 24. Write a java program to sort an ArrayList?
